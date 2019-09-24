20b24191a2570c5111bba4522353c35779cf87cf
Santiago <sagudecod97@gmail.com>
2019-09-24 22:19:20 +0000

    Created functions _strtok and tokenizer

---
+++ tokenizer.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * hsh_splitpath - Split the line wrote in the console.
+ * @value: Line wrote in console.
+ * Return: An array with all words splited.
+ */
+
+char **tokenizer(char *value)
+{
+	char **tokens;
+
+	tokens = _strtok(value);
+	if (tokens == NULL)
+	{
+		perror("shell:, Error allocating the buffer");
+		return (NULL);
+	}
+
+    return (tokens);
+}
