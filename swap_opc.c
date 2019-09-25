12f8128973e2a7ac4665d364228aa3567c030208
Santiago <sagudecod97@gmail.com>
2019-09-25 19:39:54 +0000

    Created function swap_opc

---
+++ swap_opc.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * swap_opc - Swap the two top element of stack
+ * @stack: Doubly linked list
+ * @line_number: Line's number
+ * Return: Nothing
+ */
+
+void swap_opc(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp1, *tmp2;
+	int i = 0, count = 0;
+
+	tmp1 = *stack;
+	tmp2 = *stack;
+
+	while (tmp1)
+	{
+		count++;
+		tmp1 = tmp1->next;
+	};
+	while (i < count)
+	{
+		tmp2 = tmp2->next
+	};
+	tmp1->next = tmp1->prev;
+	tmp1->prev = tmp2->prev;
+	tmp2->prev = tmp->next;
+	tmp2->next = NULL;
+}
