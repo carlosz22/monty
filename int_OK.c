b54c684867d32eb51c3845109b34bddce8d50488
Santiago <sagudecod97@gmail.com>
2019-09-25 18:03:59 +0000

    Created function int_OK to test strings

---
+++ int_OK.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * int_OK - Verifies if a string is a number
+ * @value: String to test
+ * Return: 0 on Success, -1 on Failure
+ */
+
+int int_OK(char *value)
+{
+	int i;
+
+	if (value == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; value[i] != NULL; i++)
+	{
+		if (isdigit(value[i]) == 0)
+		{
+			return (-1);
+		}
+	};
+
+	return (0);
+}
