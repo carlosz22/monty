89d5e0abee15de01be6c4f390b4d93fdb12b06f2
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-26 16:22:37 +0000

    Add check errors

---
+++ check_errors.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * check_errors - doubly linked list representation of a stack (or queue)
+ * @fp: File descriptor
+ * @buffer: Buffer from tokenizer
+ * @token_1: First token
+ * @token_2: Second token
+ * @stack: Doubly linked list
+ * @line_number: Line number
+ *
+ * Return: 0
+ */
+
+void check_errors(FILE *fp, char *buffer, char *token_1,
+char *token_2, stack_t **stack, unsigned int line_number)
+{
+	(void)fp;
+	char opcodes[] = {"push", "pall", "pop", "pint", "swap", "add", "nop"};
+	int delete = 0, i = 0;
+
+	if (strcmp(token_1, "push") == 0)
+	{
+		if (int_OK(token_2) != 0)
+		{
+			fprintf(stderr, "L%d: usage: push integer", line_number);
+			delete = 1;
+		}
+	}
+
+	for (i = 0; i < 7; i++)
+	{
+		if (strcmp(token_1, opcodes[i]) == 0 && *stack == NULL)
+		{
+			fprintf(stderr, "L%d: can't %s stack too short", line_number, opcodes[i]);
+			delete = 1;			
+		}
+	}
+
+	if (delete == 1)
+	{
+		/* close(fp); */
+		free(buffer);
+		free(stack); /* Create a function to free a linked list */
+		/* Free tokens here */
+		exit(EXIT_FAILURE);
+
+	}
+}
