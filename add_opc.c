d35ad5f71d2470b33861a5111be332d5d1ab5655
Santiago <sagudecod97@gmail.com>
2019-09-26 23:45:15 +0000

    Add if in case of stack < 2

--- add_opc.c
+++ add_opc.c
@@ -11,14 +11,23 @@ void add_opc(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 	int lastN = 0, prevN = 0, sum = 0;
+	int count = 0;
+
 	(void)line_number;
 
 	tmp = *stack;
 
 	while (tmp->next)
 	{
+		count++;
 		tmp = tmp->next;
 	};
+	if (count < 2)
+	{
+		fprintf(stderr, "L%i: can't add, stack too short\n", line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 
 	lastN = tmp->n;
 	prevN = tmp->prev->n;
