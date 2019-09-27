88127909dc22182e9a2e3252cebb66122b53fdfe
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-27 00:55:12 +0000

    Add sub opcode

---
+++ sub_opc.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * sub_opc - Substract the top two elements of the stack
+ * @stack: Doubly linked list
+ * @line_number: Line's number
+ * Return: Nothing
+ */
+
+void sub_opc(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	int lastN = 0, prevN = 0, sub = 0;
+	int count = 0;
+
+	(void)line_number;
+
+	tmp = *stack;
+
+	while (tmp->next)
+	{
+		count++;
+		tmp = tmp->next;
+	};
+	if (count < 2)
+	{
+		fprintf(stderr, "L%i: can't sub, stack too short\n", line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
+	lastN = tmp->n;
+	prevN = tmp->prev->n;
+	sub = lastN - prevN;
+	tmp->prev->n = sub;
+	tmp->prev->next = NULL;
+	free(tmp);
+}
