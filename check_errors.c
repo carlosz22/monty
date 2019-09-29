63173595139df3cd20bd1601058aa5163f990cff
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-29 22:51:09 +0000

    Fix Betty style

--- check_errors.c
+++ check_errors.c
@@ -11,10 +11,6 @@
 
 void check_errors(char *token_1, char *token_2, unsigned int line_number)
 {
-	/* char *opcodes[] = {"push", "pall", "pop", "pint", "swap", "add",
-"sub", "mul", "div", "mod", "nop", "pchar", "pstr", NULL};
-	int delete = 0, i = 0; */
-
 	if (strcmp(token_1, "push") == 0)
 	{
 		if (int_OK(token_2) != 0 || token_2 == NULL)
@@ -26,13 +22,4 @@ void check_errors(char *token_1, char *token_2, unsigned int line_number)
 	}
 	if (strcmp(token_1, "pall") == 0 && (data.stack) == NULL)
 		return;
-	/* for (i = 0; i < 13; i++)
-	{
-		if (strcmp(token_1, opcodes[i]) == 0 && (data.stack) == NULL &&
-			strcmp(token_1, "push") != 0 && strcmp(token_1, "pint") != 0)
-		{
-			fprintf(stderr, "L%d: can't %s stack too short\n", line_number, opcodes[i]);
-			delete = 1;
-		};
-	} */
 }
