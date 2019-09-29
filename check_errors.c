0462b07a72a1c8f6fe0a6ec395e4526104766c8a
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-29 22:02:09 +0000

    Fix stack if statement

--- check_errors.c
+++ check_errors.c
@@ -31,7 +31,7 @@ void check_errors(char *token_1, char *token_2, unsigned int line_number)
 	};
 	for (i = 0; i < 13; i++)
 	{
-		if (strcmp(token_1, opcodes[i]) == 0 && &(data.stack) == NULL &&
+		if (strcmp(token_1, opcodes[i]) == 0 && (data.stack) == NULL &&
 			strcmp(token_1, "push") != 0)
 		{
 			fprintf(stderr, "L%d: can't %s stack too short\n", line_number, opcodes[i]);
